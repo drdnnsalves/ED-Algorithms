d06d722b3d4276a05ce9b1d3f83c2a0795fb9ebd
Eduardo Nunes Alves <125403269+drdnnsalves@users.noreply.github.com>
2024-05-06 20:09:39 -0300

    colocadas funções selectionSort, optmizedSelectionSort, editada a main com os testes

--- bubbleSort_SelectionSort.cpp
+++ bubbleSort_SelectionSort.cpp
@@ -1,140 +1,115 @@
 #include <iostream>
 #include <chrono>
 #include <random>
-#include <cstdlib> // Incluir a biblioteca cstdlib para o uso de malloc
+#include <cstdlib>
 
 using namespace std;
 
-typedef struct Node 
-{
+typedef struct Node {
     int iPayload; 
     Node* ptrNext;
     Node* ptrPrev;
 } Node;
 
+// Funções para Lista
 Node* createNode(int);
 void displayList(Node*);
 void insertEnd(Node**, int);
 void deleteList(Node**);
 void swapValue(Node**, Node**);
-void bubbleSort(Node**, int);
-void optimizationBubbleSort(Node**, int);
+void listBubbleSort(Node**, int);
+void optimizedListBubbleSort(Node**, int);
+void listSelectionSort(Node**, int);
+void optimizedListSelectionSort(Node**, int);
 
+// Funções para array
 void swapValue(int&, int&);
 void printArray(int[], int);
 void bubbleSort(int[], int);
 void optimizationBubbleSort(int[], int);
-
 void selectionSort(int[], int);
 void optimizedSelectionSort(int[], int);
 
 int main()
 {
-    // Bubble com lista
+    srand(time(nullptr)); // Seed para números aleatórios
 
-    Node* head1 = nullptr;
-    Node* head2 = nullptr;
-    int iLength0 = 1000;
+    // Tamanhos das listas a serem testadas
+    int sizes[] = {1000, 5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000};
 
-    for (int i = 0; i < iLength0; i++)
-    {
-        int iValue =  rand() % 1000 + 1; // Gera um número aleatório entre 1 e 1000
-        insertEnd(&head1, iValue);  
-        insertEnd(&head2, iValue);
+    cout << "Teste para algorítmo BubbleSort" << endl << endl;
+
+    for (int size : sizes) {
+        Node* head1 = nullptr;
+        Node* head2 = nullptr;
+
+        // Criar listas aleatórias
+        for (int i = 0; i < size; i++) {
+            int value = rand() % 1000 + 1; // Valor aleatório entre 1 e 1000
+            insertEnd(&head1, value);
+            insertEnd(&head2, value);
+        }
+
+        // Medir o tempo de execução para listBubbleSort
+        auto start = chrono::high_resolution_clock::now();
+        listBubbleSort(&head1, size);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<double> duration = end - start;
+        cout << "Tempo para listBubbleSort          com lista de tamanho " << size << ": " << duration.count() << " segundos" << endl;
+
+        // Medir o tempo de execução para optimizedListBubbleSort
+        start = chrono::high_resolution_clock::now();
+        optimizedListBubbleSort(&head2, size);
+        end = chrono::high_resolution_clock::now();
+        duration = end - start;
+        cout << "Tempo para optimizedListBubbleSort com lista de tamanho " << size << ": " << duration.count() << " segundos" << endl;
+        
+        cout << endl;
+
+        // Limpar a memória alocada para as listas
+        deleteList(&head1);
+        deleteList(&head2);
+    }
+
+    cout << "#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#" << endl;
+    cout << "Teste para algorítmo SelectionSort" << endl << endl;
+
+    for (int size : sizes) {
+        Node* head1 = nullptr;
+        Node* head2 = nullptr;
+
+        // Criar listas aleatórias
+        for (int i = 0; i < size; i++) {
+            int value = rand() % 1000 + 1; // Valor aleatório entre 1 e 1000
+            insertEnd(&head1, value);
+            insertEnd(&head2, value);
+        }
+
+        // Medir o tempo de execução para listBubbleSort
+        auto start = chrono::high_resolution_clock::now();
+        listSelectionSort(&head1, size);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<double> duration = end - start;
+        cout << "Tempo para listSelectionSort          com lista de tamanho " << size << ": " << duration.count() << " segundos" << endl;
+
+        // Medir o tempo de execução para optimizedListBubbleSort
+        start = chrono::high_resolution_clock::now();
+        optimizedListSelectionSort(&head2, size);
+        end = chrono::high_resolution_clock::now();
+        duration = end - start;
+        cout << "Tempo para optimizedListSelectionSort com lista de tamanho " << size << ": " << duration.count() << " segundos" << endl;
+        
+        cout << endl;
+
+        // Limpar a memória alocada para as listas
+        deleteList(&head1);
+        deleteList(&head2);
     }
     
-    // Ordenando a lista usando o Bubble Sort
-    cout << "Lista original: ";
-    displayList(head1);
-
-    auto timeStart0 = chrono::high_resolution_clock::now();
-    bubbleSort(&head1, iLength0);
-    auto timeStop0 = chrono::high_resolution_clock::now();
-
-    cout << "Lista ordenada: ";
-    displayList(head1);
-    
-    auto timeDuration = chrono::duration_cast<chrono::nanoseconds>(timeStop0 - timeStart0);
-    
-    cout << "Tempo utilizado: " <<  timeDuration.count() << " nanosegundos" <<  endl;
-    
-    cout << endl;
-    
-    // Ordenando a lista usando o Bubble Sort otimizado
-
-    cout << "Lista original: ";
-    displayList(head2);
-    
-    auto timeStart1 = chrono::high_resolution_clock::now();
-    optimizationBubbleSort(&head2,  iLength0);
-    auto timeStop1 = chrono::high_resolution_clock::now();
-
-    cout << "Lista ordenada: ";
-    displayList(head2);
-    
-    auto timeDuration1 = chrono::duration_cast<chrono::nanoseconds>(timeStop1 - timeStart1);
-    
-    cout << "Tempo utilizado: " <<  timeDuration1.count() << " nanosegundos" <<  endl;
-
-    // Limpando a memória alocada para as listas
-    deleteList(&head1);
-    deleteList(&head2);
-
-
-    // Bubble com array
-    
-    int arriNumbers_1[] = {42, 4, 137, 24, 0, 9, 100, 67, 72, 431, 42, 40, 17, 14, 5, 94, 144, 671, 724, 4355};
-    int iLenght1 = 20;
-    cout << "Array original: ";
-    printArray(arriNumbers_1, iLenght1);
-    
-    auto timeStart2 = chrono::high_resolution_clock::now();
-    bubbleSort(arriNumbers_1, iLenght1);
-    auto timeStop2 = chrono::high_resolution_clock::now();
-
-    cout << "Array ordenado: ";
-    printArray(arriNumbers_1, iLenght1);
-    
-    auto timeDuration2 = chrono::duration_cast<chrono::nanoseconds>(timeStop2 - timeStart2);
-    
-    cout << "Tempo utilizado: " <<  timeDuration2.count() << " nanosegundos" <<  endl;
-    
-    cout << endl;
-
-    //Selection Sort com array
-    int arriNumbers_2[] = {42, 4, 137, 24, 0, 9, 100, 67, 72, 431};
-    int iLenght2 = 10;
-
-    auto timeStart3 = chrono::high_resolution_clock::now();
-    selectionSort(arriNumbers_2, iLenght2);
-    auto timeStop3 = chrono::high_resolution_clock::now();
-
-    cout << "Array ordenado: ";
-    printArray(arriNumbers_2, iLenght2);
-    auto timeDuration3 = chrono::duration_cast<chrono::nanoseconds>(timeStop3 - timeStart3);
-
-    cout << "Tempo utilizado: " <<  timeDuration3.count() << " nanosegundos" <<  endl;
-    cout << endl;
-
-    cout << "Array original: ";
-    printArray(arriNumbers_2, iLenght2);
-
-    auto timeStart4 = chrono::high_resolution_clock::now();
-    optimizedSelectionSort(arriNumbers_2, iLenght2);
-    auto timeStop4 = chrono::high_resolution_clock::now();
-
-    cout << "Array ordenado: ";
-    printArray(arriNumbers_2, iLenght2);
-
-    auto timeDuration4 = chrono::duration_cast<chrono::nanoseconds>(timeStop4 - timeStart4);
-    cout << "Tempo utilizado: " <<  timeDuration4.count() << " nanosegundos" <<  endl;
-
-
     return 0;
 }
 
-Node* createNode(int iValue)
-{
+Node* createNode(int iValue) {
     Node* temp = (Node*)malloc(sizeof(Node)); // Manteve o uso de malloc
     temp->iPayload = iValue;
     temp->ptrNext = nullptr;
@@ -142,54 +117,47 @@ Node* createNode(int iValue)
     
     return temp;
 }
-void displayList(Node* node)
-{
-    if (node == nullptr)
-    {
+
+void displayList(Node* node) {
+    if (node == nullptr) {
         cout << "Lista vazia: não é possível realizar displayList" << endl;
         return;
     }
     
     // se receber um nó do meio, rejeito (um modo)
-    if (node->ptrPrev != nullptr)
-    {
+    if (node->ptrPrev != nullptr) {
         cout << "O elemento está no meio ou fim da lista: não é possível realizar displayList" << endl;
         return;
     }
     
     Node* temp = node;
-
     cout << "Payload: ";
     
-    while (temp != nullptr)
-    {
+    while (temp != nullptr) {
         cout <<  temp->iPayload << " ";
         temp = temp->ptrNext;
     }
     
     cout << endl;
 }
-void insertEnd(Node** head, int iValue)
-{
+
+void insertEnd(Node** head, int iValue) {
     Node* newNode = createNode(iValue);
 
-    if (*head == nullptr) 
-    {
+    if (*head == nullptr) {
         *head = newNode; 
         return;
     }
     
     Node* temp = *head;
     while (temp->ptrNext != nullptr)
-    {
         temp = temp->ptrNext;
-    }
     
     newNode->ptrPrev = temp; // newnode aponta para o fim da lista
     temp->ptrNext = newNode; // antigo ultimo elemento aponta para o novo nó
 }
-void deleteList(Node** head) 
-{
+
+void deleteList(Node** head) {
     Node* current = *head;
     Node* next;
 
@@ -201,41 +169,37 @@ void deleteList(Node** head)
 
     *head = nullptr;
 }
+
 void swapValue(Node** Value_1, Node** Value_2) {
     int temp = (*Value_1)->iPayload;
     (*Value_1)->iPayload = (*Value_2)->iPayload;
     (*Value_2)->iPayload = temp;
 }
-void bubbleSort(Node** head, int iLength) 
-{
+
+void listBubbleSort(Node** head, int iLength) {
     Node* current = *head;
-    for (int iOuterLoop = 0; iOuterLoop < iLength - 1; iOuterLoop++)
-    {
+    
+    for (int iOuterLoop = 0; iOuterLoop < iLength - 1; iOuterLoop++) {
         current = *head; // Reinicializa o ponteiro current no início de cada iteração
-        for (int iInnerLoop = 0; iInnerLoop < iLength - iOuterLoop - 1; iInnerLoop++)
-        {
-            if (current->iPayload > current->ptrNext->iPayload)
-            {
+        for (int iInnerLoop = 0; iInnerLoop < iLength - iOuterLoop - 1; iInnerLoop++) {
+            if (current->iPayload > current->ptrNext->iPayload) {
                 swapValue(&current, &(current->ptrNext));
             }
             current = current->ptrNext;
         }
     }
 }
-void optimizationBubbleSort(Node** head, int iLength) 
-{
+
+void optimizedListBubbleSort(Node** head, int iLength) {
     Node* current = *head;
     bool bUnordered = false;
     
-    for (int iOuterLoop = 0; iOuterLoop < iLength - 1; iOuterLoop++)
-    {
+    for (int iOuterLoop = 0; iOuterLoop < iLength - 1; iOuterLoop++) {
         current = *head; // Reinicializa o ponteiro current no início de cada iteração
         bUnordered = false;
         
-        for (int iInnerLoop = 0; iInnerLoop < iLength - iOuterLoop - 1; iInnerLoop++)
-        {
-            if (current->iPayload > current->ptrNext->iPayload)
-            {
+        for (int iInnerLoop = 0; iInnerLoop < iLength - iOuterLoop - 1; iInnerLoop++) {
+            if (current->iPayload > current->ptrNext->iPayload) {
                 swapValue(&current, &(current->ptrNext));
                 bUnordered = true;
             }
@@ -248,93 +212,102 @@ void optimizationBubbleSort(Node** head, int iLength)
     }
 }
 
-void swapValue(int& irefValue_1, int& irefValue_2)
+void listSelectionSort(Node** head, int iLength) {
+    for (int iOuterLoop = 0; iOuterLoop < iLength; iOuterLoop++) {
+        Node* current = *head;
 
-{
+        for (int iInnerLoop = iOuterLoop + 1; iInnerLoop < iLength; iInnerLoop++) {
+            if (current->iPayload > current->ptrNext->iPayload) {
+                swapValue(&current, &(current->ptrNext));
+            }
+            current = current->ptrNext;
+        }
+    }
+}
+
+void optimizedListSelectionSort(Node** head, int iLength) {
+    Node *current; 
+    Node *min;
+
+    for (int i = 0; i < iLength - 1; i++) {
+        current = *head;
+        min = current;
+
+        for (int j = i + 1; j < iLength; j++) {
+            if (current->iPayload > current->ptrNext->iPayload) {
+                min = current->ptrNext;
+            }
+            current = current->ptrNext;
+        }
+
+        if (min != current) {
+            swapValue(&min, &current);
+        }
+    }
+}
+
+void swapValue(int& irefValue_1, int& irefValue_2) {
     int iTemp = irefValue_1;
     irefValue_1 = irefValue_2;
     irefValue_2 = iTemp;
     
 }
-void printArray(int arriNumbers[], int iLenght)
 
-{
+void printArray(int arriNumbers[], int iLenght) {
     for(int i = 0; i < iLenght; i++) cout<<arriNumbers[i] << " ";
     cout << endl;
 }
-void bubbleSort(int arriNumbers[], int iLenght) 
-{
+
+void bubbleSort(int arriNumbers[], int iLenght) {
     
     for (int iOuterLoop = 0; iOuterLoop < iLenght-1; iOuterLoop++)
-    {
         for (int iInnerLoop = 0; iInnerLoop < iLenght-1; iInnerLoop++)
-        {
             if (arriNumbers[iInnerLoop] > arriNumbers[iInnerLoop+1])
-            {
                 swapValue(arriNumbers[iInnerLoop], arriNumbers[iInnerLoop+1]);
-            }    
-        }
-
-    }
 }
-void otimizationBubbleSort(int arriNumbers[], int iLenght) 
-{
+
+void otimizationBubbleSort(int arriNumbers[], int iLenght) {
     bool bUnordered = false;
     
-    for (int iOuterLoop = 0; iOuterLoop < iLenght-1; iOuterLoop++)
-    {
+    for (int iOuterLoop = 0; iOuterLoop < iLenght-1; iOuterLoop++) {
         bUnordered = false;
         
-        for (int iInnerLoop = 0; iInnerLoop < iLenght-1-iOuterLoop; iInnerLoop++)
-        {
-            if (arriNumbers[iInnerLoop] > arriNumbers[iInnerLoop+1])
-            {
+        for (int iInnerLoop = 0; iInnerLoop < iLenght-1-iOuterLoop; iInnerLoop++) {
+            if (arriNumbers[iInnerLoop] > arriNumbers[iInnerLoop+1]) {
                 swapValue(arriNumbers[iInnerLoop], arriNumbers[iInnerLoop+1]);
                 bUnordered = true;
-                
             }    
             
-            if(bUnordered == false) {
+            if(bUnordered == false)
                 break;
-            }
         }
 
     }
 }
 
-void selectionSort(int arriNumbers[], int iLenght)
-{
+void selectionSort(int arriNumbers[], int iLenght) {
+    
     for (int iOuterLoop = 0; iOuterLoop < iLenght; iOuterLoop++)
-    {
         for (int iInnerLoop = iOuterLoop+1; iInnerLoop < iLenght; iInnerLoop++)
-        {
             if(arriNumbers[iOuterLoop] > arriNumbers[iInnerLoop])
-            {
                 swapValue(arriNumbers[iOuterLoop], arriNumbers[iInnerLoop]);
-            }
-        }
-
-    }
 }
-void optimizedSelectionSort(int arriNumbers[], int iLenght)
-{
+
+void optimizedSelectionSort(int arriNumbers[], int iLenght) {
     int iminValue = 0;
     int iSwapIndex = 0;
     
-    for (int iOuterLoop = 0; iOuterLoop < iLenght; iOuterLoop++)
-    {
+    for (int iOuterLoop = 0; iOuterLoop < iLenght; iOuterLoop++) {
         iminValue = arriNumbers[iOuterLoop];
         iSwapIndex = iOuterLoop;
-        for (int iInnerLoop = iOuterLoop+1; iInnerLoop < iLenght; iInnerLoop++)
-        {
-            if(iminValue > arriNumbers[iInnerLoop])
-            {
+        
+        for (int iInnerLoop = iOuterLoop+1; iInnerLoop < iLenght; iInnerLoop++) {
+            if(iminValue > arriNumbers[iInnerLoop]) {
                 iminValue = arriNumbers[iInnerLoop];
                 iSwapIndex = iInnerLoop;
             }
         }
-        // colocar if pra ver se iOuterLoop é diferente de iSwapIndex pra
-        //não trocar um elemento com ele mesmo
+        
         swapValue(arriNumbers[iOuterLoop], arriNumbers[iSwapIndex]);
     }
 }
